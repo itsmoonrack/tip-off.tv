11f538d4434351b8956a94b80ca1eec7650bd839
sylvainlecoy <sylvain.lecoy@gmail.com>
2012-10-19 22:25:45 +0200

    Initial commit.

---
+++ src/tv/tipoff/api/TipOff_TVServlet.java
@@ -0,0 +1,13 @@
+package tv.tipoff.api;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TipOff_TVServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
