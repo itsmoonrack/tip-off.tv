6d046bdae08f69f0522a2e33c5c516f42a4513bc
sylvainlecoy <sylvain.lecoy@gmail.com>
2012-10-20 19:20:30 +0200

    Avec les API google.

--- src/tv/tipoff/services/pgep/RESTService.java
+++ src/tv/tipoff/services/pgep/RESTService.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -72,33 +74,18 @@ public class RESTService implements PGEPService {
 
 	@Override
 	public List<Program> getPrograms() {
-		URIBuilder builder = new URIBuilder();
-		builder.setScheme(scheme)
-			.setHost(host)
-			.setPath("/programs")
-			.setParameter("format", "json");
-		
 		try {
-			URI uri = builder.build();
-			HttpGet request = new HttpGet(uri);
-			HttpResponse response = client.execute(request);
-			HttpEntity entity = response.getEntity();
-			
-			if (entity != null) {
-				InputStream stream = entity.getContent();
-				InputStreamReader reader = new InputStreamReader(stream);
-				Type type = new TypeToken<Results<Program>>() {}.getType();
-				Results<Program> results = gson.fromJson(reader, type);
-				return results.getResults();
-				
-			}
-		} catch (URISyntaxException e) {
+			URL url = new URL("http://pgep.francetv.fr/programs?format=json");
+			InputStreamReader reader = new InputStreamReader(url.openStream());
+			Type type = new TypeToken<Results<Program>>() {}.getType();
+			Results<Program> results = gson.fromJson(reader, type);
+			return results.getResults();
+		} catch (MalformedURLException e) {
 			throw new RuntimeException(e);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 		return null;
 	}
 
